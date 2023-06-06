7cd89daa2c9bf5e89249f55cbb06411ff8040b18
AxelVarreira <axel.felix.varreira2@gmail.com>
2023-06-06 15:18:48 -0300

    [Feat]: adding validators

--- main.py
+++ main.py
@@ -1,38 +1,52 @@
 # This is a sample Python script.
 
-import sys, getopt
+import sys, getopt, argparse
+from types import NoneType
 
-argumentList = sys.argv[1:]
+from validators import validate_arguments
 
-# Opcoes que tu podera passar na hora de rodar o script. eg: python3 main.py -a
-options = "hmka:"
+# argumentList = sys.argv[1:]
+#
+# # Opcoes que tu podera passar na hora de rodar o script. eg: python3 main.py -a
+# options = "hmka:"
+#
+# long_options = ["Help", "File", "Key", "Action"]
+#
+# try:
+#     arguments, values = getopt.getopt(argumentList, options, long_options)
+#
+#     for currentArgument, currentValue in arguments:
+#
+#         if currentArgument in ("-h", "--Help"):
+#             print("Welcome stranger!\n")
+#             print("to run this script you need to run: \n")
+#             print("# python3 main.py <arquivo de texto> <chave de cifragem> <decrypt ou incrypt>")
+#
+#         elif currentArgument in ("-m", "--File"):
+#             print("Displaying file_name:", sys.argv[0])
+#
+#         elif currentArgument in ("-o", "--Output"):
+#             print(("Enabling special output mode (% s)") % (currentValue))
+#
+#         elif currentArgument in ("-k", "--Key"):
+#             if len(currentValue) == 0:
+#                 raise ValueError("Chave nao pode estar vazia!")
+#
+#         elif currentArgument in ("-a", "--Action"):
+#             if currentValue != "decrypt" or currentValue != "encrypt":
+#                 raise ValueError("Valores de acao invalidos")
+#
+# except getopt.error as err:
+#     # output error, and return with an error code
+#     print(str(err))
 
-long_options = ["Help", "File", "Key", "Action"]
+parser = argparse.ArgumentParser()
 
-try:
-    arguments, values = getopt.getopt(argumentList, options, long_options)
+parser.add_argument("--a", "--Action", help="Actions: decrypt or encrypt file")
+parser.add_argument("--k", "--Key", help="Actions: key to encrypt or decrypt file")
+parser.add_argument("--f", "--File", help="Actions: File to be encrypt or decrypt")
 
-    for currentArgument, currentValue in arguments:
+args = parser.parse_args()
 
-        if currentArgument in ("-h", "--Help"):
-            print("Welcome stranger!\n")
-            print("to run this script you need to run: \n")
-            print("# python3 main.py <arquivo de texto> <chave de cifragem> <decrypt ou incrypt>")
+validate_arguments.validate_arguments(args.k, args.a, args.f)
 
-        elif currentArgument in ("-m", "--File"):
-            print("Displaying file_name:", sys.argv[0])
-
-        elif currentArgument in ("-o", "--Output"):
-            print(("Enabling special output mode (% s)") % (currentValue))
-
-        elif currentArgument in ("-k", "--Key"):
-            if len(currentValue) == 0:
-                raise ValueError("Chave nao pode estar vazia!")
-
-        elif currentArgument in ("-a", "--Action"):
-            if currentValue != "decrypt" or currentValue != "encrypt":
-                raise ValueError("Valores de acao invalidos")
-
-except getopt.error as err:
-    # output error, and return with an error code
-    print(str(err))
